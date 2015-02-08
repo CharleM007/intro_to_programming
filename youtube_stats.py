218aad9aa46f900429c7bd65409e1ff12f2bcd2e
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-02-07 22:13:42 -0600

    updated youtube_stats.py for students

--- youtube_stats.py
+++ youtube_stats.py
@@ -1,3 +1,19 @@
+print(str.upper("These results come from nine lines of code with stock Python 3"))
+import json
+from urllib.request import urlopen
+url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json" 
+response = urlopen(url)
+contents = response.read()
+text = contents.decode('utf8')
+data = json.loads(text)
+for video in data['feed']['entry'][0:5]:
+    print(video['title']['$t'])
+
+
+print(20*"*")
+print(str.upper('''These results come from six lines of Python 3, 
+    using the Requests library. There's also some additional formatting
+    done to the strings. That's in line 23 of the code.'''))
 import requests
 url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json" 
 response = requests.get(url)
@@ -6,21 +22,3 @@ for video in data['feed']['entry'][0:5]:
     view_count = int(video['yt$statistics']['viewCount']) #converts viewCount into an int
     print(video['title']['$t'], "with", '{:,}'.format(view_count), "views") #second 1/2 formats the int with thousands separators
 
-
-
-'''print()
-print("NEW CODE STARTS HERE!!!")
-print('''This is showing me the results from the "data = response.json"; the type and length of each
-    element in "data['feed']['entry']" ''')
-#this shows me the various data catagories stored in each YouTube Entry listing
-f = open("youTube_data.txt", "a")
-
-for i in data['feed']['entry']:
-    # for key in i:
-    #     print (key, )
-    # print (i['yt$statistics']['viewCount'])
-    f.write(str(type(i)) + " " + str(len(i)) + " " + i['title']['$t'] + "\n" + str(i) + "\n" + "\n")
-    print (len(i), i['title']['$t'])
-#Looks like I'm after ['title']['$t'] and ['yt$statistics']
-'''
-f.close()
